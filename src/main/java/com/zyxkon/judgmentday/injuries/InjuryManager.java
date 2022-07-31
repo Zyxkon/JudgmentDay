6f64c424167d56117508aee5a736c151ab5c1c98
Zyxkon <quangglider@gmail.com>
2022-07-31 22:48:28 +0700

    removed getHashmap()

--- src/main/java/com/zyxkon/judgmentday/injuries/InjuryManager.java
+++ src/main/java/com/zyxkon/judgmentday/injuries/InjuryManager.java
@@ -2,11 +2,9 @@ package com.zyxkon.judgmentday.injuries;
 
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
 import java.util.UUID;
 
 public abstract class InjuryManager<I> {
-    public abstract HashMap<UUID, I> getHashmap();
     public abstract void put(UUID uuid, I process);
     public abstract I getInjury(UUID uuid);
     public boolean isInjured(Player player){
